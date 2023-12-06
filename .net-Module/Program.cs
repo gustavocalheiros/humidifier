8086cc819ee4f6b2ade421d4f55eb4e83a44d08b
Gustavo Calheiros <gustavorlc@gmail.com>
2023-12-06 16:02:31 +0100

    initial files of .Net Module

---
+++ .net-Module/Program.cs
@@ -0,0 +1,22 @@
+﻿using Azure.Data.Tables;
+using WeatherStats;
+
+var endPoint = "https://weatherstats.table.core.windows.net/stats";
+var accountKey = "kjq19H9Dy0JROz3cciwjxEYWmTgLdT1cZmWVxSnD3OuxMJtsGWnwAG/OKx1ZgbzwtV2fjkpMdqHK+AStFp6+gQ==";
+var accountName = "weatherstats";
+var partitionKey = "Temperature_Humidity";
+var tableName = "stats";
+
+var serviceClient = new TableServiceClient(
+    new Uri(endPoint),
+    new TableSharedKeyCredential(accountName, accountKey));
+
+var temperature = new WeatherInfo(partitionKey, "0", 12, 13);
+
+var tableClient = serviceClient.GetTableClient(tableName);
+
+var ifNotExists = tableClient.CreateIfNotExists();
+
+var upsertEntity = tableClient.UpsertEntity(temperature);
+
+int x = 10;
---
+++ .net-Module/WeatherInfo.cs
@@ -0,0 +1,27 @@
+﻿namespace WeatherStats;
+
+using Azure;
+using Azure.Data.Tables;
+
+public class WeatherInfo : ITableEntity
+{
+    public WeatherInfo(string partitionKey, string rowKey, int temperature, int humidity)
+    {
+        PartitionKey = partitionKey;
+        RowKey = rowKey;
+        Temperature = temperature;
+        Humidity = humidity;
+    }
+
+    public string PartitionKey { get; set; }
+    
+    public string RowKey { get; set; }
+    
+    public DateTimeOffset? Timestamp { get; set; }
+    
+    public ETag ETag { get; set; }
+    
+    public int Temperature { get; }
+    
+    public int Humidity { get; }
+}
