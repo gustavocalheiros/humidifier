a226a0e072a738630c15943fa9a2e439564a2d23
Gustavo Calheiros <gustavorlc@gmail.com>
2023-12-20 12:58:27 +0100

    introduced converter from Python time (seconds) to DateTime (C#)

--- .net-Module/Models/WeatherInfoContext.cs
+++ .net-Module/Models/WeatherInfoContext.cs
@@ -4,6 +4,8 @@ using Microsoft.EntityFrameworkCore;
 
 public class WeatherInfoContext : DbContext
 {
+    private static readonly DateTime UnitTimeZero = new(1970, 1, 1, 0, 0, 0, 0);
+
     public DbSet<WeatherInfoEF> WeatherInfos { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
@@ -13,6 +15,12 @@ public class WeatherInfoContext : DbContext
 
     protected override void OnModelCreating(ModelBuilder modelbuilder)
     {
+        // Id auto-increment
         modelbuilder.Entity<WeatherInfoEF>().Property(p => p.Id).ValueGeneratedOnAdd();
+
+        // seconds to DateTime
+        modelbuilder.Entity<WeatherInfoEF>().Property(p => p.Time).HasConversion(
+            v => v.Second,
+            v => UnitTimeZero.AddSeconds(v));
     }
 }
