b3a6560d4d3dc2de052c40b22990bab8f4d872c0
Gustavo Calheiros <gustavorlc@gmail.com>
2023-12-21 10:59:10 +0100

    Added Strongname (signing assembly)

---
+++ .net-Module/WeatherStats/InternalsVisibleTo.cs
@@ -0,0 +1,8 @@
+﻿using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("WeatherStats_Tests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100850f6beeeba020" +
+                              "e2a68ea36a1f9314e00334c6a055e30eff04811c7038344f842affdead23e034d38ee133f06bd1" +
+                              "9d2069bfdee8be3be082777ca1d5bc7de969f905c62cdd7628c19feb42ddfaae3de768255af69a" +
+                              "968897e0ae74a1b1c44c3c6c89d256229d6f30a7c350c37d7a2e7d24e2d232b31155ef790bf4e9" +
+                              "0a8d26b5")]
+
--- .net-Module/WeatherStats/KeyVault/KeyVault.cs
+++ .net-Module/WeatherStats/KeyVault/KeyVault.cs
@@ -1,17 +1,4 @@
-﻿using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("WeatherStats_Tests, PublicKey=002400000480000094" +
-                              "0000000602000000240000525341310004000" +
-                              "001000100bf8c25fcd44838d87e245ab35bf7" +
-                              "3ba2615707feea295709559b3de903fb95a93" +
-                              "3d2729967c3184a97d7b84c7547cd87e435b5" +
-                              "6bdf8621bcb62b59c00c88bd83aa62c4fcdd4" +
-                              "712da72eec2533dc00f8529c3a0bbb4103282" +
-                              "f0d894d5f34e9f0103c473dce9f4b457a5dee" +
-                              "fd8f920d8681ed6dfcb0a81e96bd9b176525a" +
-                              "26e0b3")]
-
-namespace WeatherStats.KeyVault;
+﻿namespace WeatherStats.KeyVault;
 
 using Azure.Identity;
 using Azure.Security.KeyVault.Secrets;
