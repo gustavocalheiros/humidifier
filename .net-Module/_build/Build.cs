a209c3a966f7049e2384112a1ed102f3072d45a1
Gustavo Calheiros <gustavorlc@gmail.com>
2023-12-21 16:34:00 +0100

    Configured NUKE build

--- .net-Module/_build/Build.cs
+++ .net-Module/_build/Build.cs
@@ -1,44 +1,57 @@
-using System;
-using System.Linq;
 using Nuke.Common;
-using Nuke.Common.CI;
-using Nuke.Common.Execution;
-using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
-using Nuke.Common.Tooling;
-using Nuke.Common.Utilities.Collections;
-using static Nuke.Common.EnvironmentInfo;
-using static Nuke.Common.IO.FileSystemTasks;
-using static Nuke.Common.IO.PathConstruction;
+using Nuke.Common.Tools.DotNet;
+
+using static Nuke.Common.Tools.DotNet.DotNetTasks;
 
 class Build : NukeBuild
 {
-    /// Support plugins are available for:
-    ///   - JetBrains ReSharper        https://nuke.build/resharper
-    ///   - JetBrains Rider            https://nuke.build/rider
-    ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
-    ///   - Microsoft VSCode           https://nuke.build/vscode
-
     public static int Main () => Execute<Build>(x => x.Compile);
 
     [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
     readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
 
+    [Solution]
+    readonly Solution Solution;
+
+    Project TestProject;
+
+    Target Init => _ => _
+        .Before(Restore)
+        .Executes(() =>
+        {
+            TestProject = Solution.GetProject("WeatherStats_Tests");
+        });
+
     Target Clean => _ => _
         .Before(Restore)
         .Executes(() =>
         {
+            DotNetClean();
         });
 
     Target Restore => _ => _
         .Executes(() =>
         {
+            DotNetRestore(x => x.SetProjectFile(Solution));
         });
 
     Target Compile => _ => _
         .DependsOn(Restore)
         .Executes(() =>
         {
+            DotNetBuild(x => x
+                .SetProjectFile(Solution)
+                .SetConfiguration(Configuration));
         });
 
+    Target UnitTests => _ => _
+        .DependsOn(Compile)
+        .DependsOn(Init)
+        .Executes(() =>
+        {
+            DotNetTest(x => x
+                .SetProjectFile(TestProject)
+                .SetConfiguration(Configuration));
+        });
 }
